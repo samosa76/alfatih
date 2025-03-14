0057ac66d6c9962454a2b2aaca78e368e3e7b459
Sam_76 <muhammadusamah76@gmail.com>
2025-03-15 04:18:18 +0700

    make new news

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
 //Silahkan hapus file default.php dari folder public_html sebelum Anda mengupload file website Anda
-include_once dirname(__FILE__) . '/home.html';
+include_once dirname(__FILE__) . '/news.html';
 
 ?>
